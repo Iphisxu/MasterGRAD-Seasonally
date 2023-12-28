93670a9f240b6046113eb11c4fbbfd36c27b6c05
Evan <iphisxu@gmail.com>
2023-12-28 21:33:14 +0800

    改正了气象观测boxplot和随机森林bar图选错了PRD范围的错误

--- src/namelist.py
+++ src/namelist.py
@@ -66,7 +66,8 @@ meicdata = datadir + 'MEIC/'
 geobdydir = datadir + 'shapefile/cities_geobdy/'
 admindir = datadir + 'shapefile/cities_admin/'
 
-city_names = ['PRD', 'Guangzhou', 'Foshan', 'Zhongshan', 'Zhuhai', 'Zhaoqing', 'Jiangmen',
+city_names = ['PRD', 'PRD_merge', 'Guangzhou', 'Foshan',
+              'Zhongshan', 'Zhuhai', 'Zhaoqing', 'Jiangmen',
               'Dongguan', 'Shenzhen', 'Huizhou', 'Hongkong', 'Macau']
 
 shp_files = {}
@@ -80,29 +81,3 @@ for city_name in city_names:
 for city_name in city_names:
     adm_path = admindir + f'{city_name}/{city_name}.shp'
     shp_files[f'{city_name}_adm'] = adm_path
-
-# shp_PRD_geo       = geobdydir + 'PRD/PRD.shp'
-# shp_Guangzhou_geo = geobdydir + 'Guangzhou/Guangzhou.shp'
-# shp_Foshan_geo    = geobdydir + 'Foshan/Foshan.shp'
-# shp_Zhongshan_geo = geobdydir + 'Zhongshan/Zhongshan.shp'
-# shp_Zhuhai_geo    = geobdydir + 'Zhuhai/Zhuhai.shp'
-# shp_Zhaoqing_geo  = geobdydir + 'Zhaoqing/Zhaoqing.shp'
-# shp_Jiangmen_geo  = geobdydir + 'Jiangmen/Jiangmen.shp'
-# shp_Dongguan_geo  = geobdydir + 'Dongguan/Dongguan.shp'
-# shp_Shenzhen_geo  = geobdydir + 'Shenzhen/Shenzhen.shp'
-# shp_Huizhou_geo   = geobdydir + 'Huizhou/Huizhou.shp'
-# shp_Hongkong_geo  = geobdydir + 'Hongkong/Hongkong.shp'
-# shp_Macau_geo     = geobdydir + 'Macau/Macau.shp'
-
-# shp_PRD_adm       = admindir + 'PRD/PRD.shp'
-# shp_Guangzhou_adm = admindir + 'Guangzhou/Guangzhou.shp'
-# shp_Foshan_adm    = admindir + 'Foshan/Foshan.shp'
-# shp_Zhongshan_adm = admindir + 'Zhongshan/Zhongshan.shp'
-# shp_Zhuhai_adm    = admindir + 'Zhuhai/Zhuhai.shp'
-# shp_Zhaoqing_adm  = admindir + 'Zhaoqing/Zhaoqing.shp'
-# shp_Jiangmen_adm  = admindir + 'Jiangmen/Jiangmen.shp'
-# shp_Dongguan_adm  = admindir + 'Dongguan/Dongguan.shp'
-# shp_Shenzhen_adm  = admindir + 'Shenzhen/Shenzhen.shp'
-# shp_Huizhou_adm   = admindir + 'Huizhou/Huizhou.shp'
-# shp_Hongkong_adm  = admindir + 'Hongkong/Hongkong.shp'
-# shp_Macau_adm     = admindir + 'Macau/Macau.shp'
